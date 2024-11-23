f189690faa4434406ffb3ff670acf26be2df7fa5
Abdul Hanan <hananaslam90@gmail.com>
2024-11-23 11:13:22 +0500

    Initial Files For Setup

---
+++ LibApp/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using LibApp.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibApp.Data
+{
+    public class AppDbContext : IdentityDbContext<Users>
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
